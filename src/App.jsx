d7b3d822230cf489f5e1006468eec21dcb802f64
Sayed Ali Sena <131773898+Alisenaqubadi@users.noreply.github.com>
2025-11-07 12:50:32 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./css/App.css";
 
 function App() {
-    const [heading, setHeading] = useState("Magnificent Monkeys");
-
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
-
-  return (
-    <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
-  );
+  return (<></>);
 }
 
 export default App;
